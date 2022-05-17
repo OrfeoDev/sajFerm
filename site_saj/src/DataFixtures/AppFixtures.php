2c47809983a66159d3e8e6475dc1570647f5a97d
OrfeoDev <orfe34@icloud.com>
2022-05-17 18:21:03 +0200

    Frontend modification du h1,installation de fixture et integration de fausse donnee prospect

--- site_saj/src/DataFixtures/AppFixtures.php
+++ site_saj/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Prospect;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,6 +10,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        for ($i = 0; $i<10 ; $i++)
+        {
+            $prospect = new Prospect();
+            $prospect->setNom('prospect'.$i)
+            ->setPrenom('prospect')
+            ->setEmail('prospect@.com'.$i)
+            ->setTelephone("046987569")
+            ->setDemandeDeDevis("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+            $manager->persist($prospect);
+        }
         // $product = new Product();
         // $manager->persist($product);
 
